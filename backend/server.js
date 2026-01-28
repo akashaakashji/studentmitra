cd615b212a0a8671d7f84036cf04522a2d87b1f5
akashaakashji <aav077075@gmail.com>
2026-01-28 18:53:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
 import driveRoutes from "./routes/driveRoutes.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/drive", driveRoutes);
 
 export default app;
+
